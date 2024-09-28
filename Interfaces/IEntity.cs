94a207ab5d93488951ae22e9227b612e00e47147
Samantha A Loveday <sloveday1@my.wctc.edu>
2024-09-27 22:27:56 -0500

    Update IEntity.cs

--- Interfaces/IEntity.cs
+++ Interfaces/IEntity.cs
@@ -1,10 +1,9 @@
-﻿namespace W5_assignment_template.Interfaces
+namespace W5_assignment_template.Interfaces
 {
     public interface IEntity
     {
         void Attack(IEntity target);
         void Move();
-        void Fly();
         string Name { get; set; }
     }
 
