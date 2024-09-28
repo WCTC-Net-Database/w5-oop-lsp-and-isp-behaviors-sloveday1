12d38c480259d058494bb156f41b2b3d594ea4ce
Samantha A Loveday <sloveday1@my.wctc.edu>
2024-09-27 22:22:03 -0500

    Update GameEngine.cs

--- Services/GameEngine.cs
+++ Services/GameEngine.cs
@@ -1,4 +1,4 @@
-﻿using W5_assignment_template.Interfaces;
+using W5_assignment_template.Interfaces;
 using W5_assignment_template.Models;
 
 namespace W5_assignment_template.Services
@@ -8,12 +8,18 @@ namespace W5_assignment_template.Services
         private readonly IEntity _character;
         private readonly IEntity _goblin;
         private readonly IEntity _ghost;
+        private readonly IEntity _giant;
+        private readonly IEntity _healer;
+        private readonly IEntity _archer;
 
-        public GameEngine(IEntity character, IEntity goblin, IEntity ghost)
+        public GameEngine(IEntity character, IEntity goblin, IEntity ghost, IEntity giant, IEntity healer, IEntity archer)
         {
             _character = character;
             _goblin = goblin;
             _ghost = ghost;
+            _giant = giant;
+            _healer = healer;
+            _archer = archer;
         }
 
         public void Run()
@@ -21,16 +27,29 @@ namespace W5_assignment_template.Services
             _character.Name = "Hero";
             _goblin.Name = "Goblin";
             _ghost.Name = "Ghost";
+            _giant.Name = "Giant";
+            _healer.Name = "Healer";
+            _archer.Name = "Archer";
 
             _character.Move();
-            _character.Attack(_goblin);
-
             _goblin.Move();
+            _character.Attack(_goblin);
             _goblin.Attack(_character);
-
+            
             _ghost.Move();
             _ghost.Attack(_character);
             ((Ghost) _ghost).Fly();
+            
+            _healer.Move();
+            _healer.Attack(_goblin);
+            ((Healer) _healer).Heal(_character);
+            
+            _giant.Move();
+            ((Giant) _giant).Throw(_healer);
+            
+            _archer.Move();
+            ((Archer) _archer).Shoot(_giant);
+            
         }
     }
 }
