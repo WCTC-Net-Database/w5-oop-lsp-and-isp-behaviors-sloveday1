8864645ee9f04adb1b86ea16aeb6c409b7cc36cb
Samantha A Loveday <sloveday1@my.wctc.edu>
2024-09-27 22:21:17 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using W5_assignment_template.Interfaces;
 using W5_assignment_template.Models;
 using W5_assignment_template.Services;
@@ -12,8 +12,11 @@ namespace W5_assignment_template
             var character = new Character();
             var goblin = new Goblin();
             var ghost = new Ghost();
+            var giant = new Giant();
+            var healer = new Healer();
+            var archer = new Archer();
 
-            var gameEngine = new GameEngine(character, goblin, ghost);
+            var gameEngine = new GameEngine(character, goblin, ghost, giant, healer, archer);
             gameEngine?.Run();
         }
 
